e42575ebb7475ea9227f703150ade63cf0be6da7
epop48 <37502068+epop48@users.noreply.github.com>
2018-09-03 10:55:58 +0800

    Add files via upload

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function login(){
+        return view('pages.login');
+    }
+
+    public function dashboard(){
+        return view('pages.dashboard');
+    }
+
+    public function channel(){
+        return view('pages.channel');
+    }
+
+    public function table(){
+        return view('pages.table');
+    }
+}
